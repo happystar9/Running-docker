7ddef3848c1b8d47eb08e78acbb43773d4fc0169
happy <ricardo_ruizjr@hotmail.com>
2024-11-06 00:49:21 -0800

    Changed some things

---
+++ TetrisApi/Services/Chatservice.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TetrisApi.Data;
+using TetrisShared.DTOs;
+
+namespace TetrisApi.Services
+{
+    public class ChatService(Dbf25TeamArzContext dbcontext) : IChatService
+    {
+
+        public Task<ChatDto> GetChatAsync(Chat chat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ChatDto> PostChatAsync(Chat chat)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
+public interface IChatService
+{
+    Task<ChatDto> GetChatAsync(Chat chat);
+    Task<ChatDto> PostChatAsync(Chat chat);
+}
