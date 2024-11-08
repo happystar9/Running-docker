a5de04f37a9c131e97d0d8eac57c8c8f47e9a07a
Zane Clegg <zaner.clegg@gmail.com>
2024-11-08 11:26:42 -0700

    Added unimplemented endpoints to TetrisController

---
+++ TetrisWeb/Api/Controllers/TetrisController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TetrisWeb.ApiServices;
+using TetrisShared.DTOs;
+using TetrisWeb.Components.Models;
+
+
+
+namespace TetrisWeb.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TetrisController(IPlayerService playerService) : ControllerBase
+{
+    [HttpGet("boardState")]
+    public async Task<CellList> BoardState()
+    {
+        
+    }
+
+    [HttpPost("moveRight/{x}")]
+    public async Task MoveRight(int x)
+    {
+
+    }
+
+    [HttpPost("moveLeft/{x}")]
+    public async Task MoveLeft(int x)
+    {
+
+    }
+
+    [HttpPost("rotate")]
+    public async Task Rotate()
+    {
+
+    }
+
+    [HttpPost("drop")]
+    public async Task Drop()
+    {
+
+    }
+}
