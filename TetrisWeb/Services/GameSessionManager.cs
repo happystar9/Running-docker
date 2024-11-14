362ecbd3bad179858c1209989adad96ece3c356b
Audrey Inglish <audreyinglish@gmail.com>
2024-11-13 19:54:24 -0700

    can get user ids in the profile page

---
+++ TetrisWeb/Services/GameSessionManager.cs
@@ -0,0 +1,27 @@
+﻿using TetrisWeb.ApiServices;
+using TetrisWeb.ApiServices.Interfaces;
+using TetrisWeb.Services;
+
+namespace TetrisWeb.Services
+{
+    public class GameSessionManager(IApiKeyManagementService keyManager)
+    {
+        public readonly Dictionary<string, GameStateService> _games = new();
+
+        public async Task<GameStateService> CreateGame(int playerId)
+        {
+            var apiKey = await keyManager.AssignKeyAsync(playerId);
+            if (!_games.TryGetValue(apiKey, out var gameService))
+            {
+                gameService = new GameStateService();
+                _games[apiKey] = gameService;
+            }
+            return gameService;
+        }
+
+        public void EndGame(string playerId)
+        {
+            _games.Remove(playerId);
+        }
+    }
+}
