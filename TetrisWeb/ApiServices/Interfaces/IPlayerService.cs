437c546120d1008be07eaa44e0332b7d58e402b9
Audrey Inglish <audreyinglish@gmail.com>
2024-11-13 21:29:51 -0700

    need to debug the refactored UserProfile and Register pages

--- TetrisWeb/ApiServices/Interfaces/IPlayerService.cs
+++ TetrisWeb/ApiServices/Interfaces/IPlayerService.cs
@@ -4,6 +4,7 @@ namespace TetrisWeb.ApiServices.Interfaces;
 public interface IPlayerService
 {
     Task<PlayerDto> GetPlayerByAuthIdAsync(string authId);
+    Task<PlayerDto> GetPlayerByUsernameAsync(string username);
     Task<PlayerDto> CreatePlayerAsync(PlayerDto player);
     Task<PlayerDto> UpdatePlayerAsync(PlayerDto player);
     Task<int> GetPlayerTotalScore(string authId);
