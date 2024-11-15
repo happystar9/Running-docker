ca7b3e6a166b434c87a97f95c72f98476693038d
Audrey Inglish <audreyinglish@gmail.com>
2024-11-14 20:25:41 -0700

    passing tetromino test

--- TetrisTest/UnitTests.cs
+++ TetrisTest/UnitTests.cs
@@ -1,11 +1,27 @@
-namespace TetrisTest
+using FluentAssertions;
+using TetrisWeb.Components.Models;
+namespace TetrisTest;
+
+public class TetrominoTests
 {
-	public class UnitTest1
+	[Fact]
+	public void Tetromino_Should_Move_Right_When_Not_At_Edge()
 	{
-		//[Fact]
-		//public void Test1()
-		//{
-		//	throw new NotImplementedException();
-		//}
-	}
+		var grid = new Grid();
+		CellList coveredCells = new();
+		var tetromino = new TestTetromino(grid, coveredCells);
+		tetromino.CenterPieceCol = 2;
+
+		tetromino.MoveRight();
+
+		tetromino.CenterPieceCol.Should().Be(3);
+    }
+}
+
+public class TestTetromino : Tetromino
+{
+    public TestTetromino(Grid grid, CellList coveredCells) : base(grid)
+    {
+        CoveredCells = coveredCells;
+    }
 }
