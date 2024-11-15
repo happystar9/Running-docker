8f0b99e73451d64505dfaa369c848335ca945d14
zane Clegg <zane.clegg@students.snow.edu>
2024-11-15 13:07:10 -0700

    Fixed the gamesessionmanager

--- TetrisWeb/Services/GameSessionManager.cs
+++ TetrisWeb/Services/GameSessionManager.cs
@@ -1,24 +1,33 @@
 ﻿using TetrisWeb.ApiServices;
 using TetrisWeb.ApiServices.Interfaces;
+using TetrisWeb.DTOs;
+using TetrisWeb.GameData;
 using TetrisWeb.Services;
 
 namespace TetrisWeb.Services
 {
     public class GameSessionManager(IApiKeyManagementService keyManager)
     {
-        public readonly Dictionary<string, GameStateService> _games = new();
+        GameSessionDto game;
+        public readonly Dictionary<string, GameSessionService> _games = new();
 
-        public async Task<GameStateService> CreateGame(int playerId)
+        public async Task<GameSessionService> CreateGame(int playerId)
         {
             var apiKey = await keyManager.AssignKeyAsync(playerId);
             if (!_games.TryGetValue(apiKey, out var gameService))
             {
-                gameService = new GameStateService();
+                gameService = new GameSessionService(game);
                 _games[apiKey] = gameService;
             }
             return gameService;
         }
 
+        public async Task<GameSessionService> CreateGame()
+        {
+            var gameService = new GameSessionService(game);
+
+            return gameService;
+        }
         public void EndGame(string playerId)
         {
             _games.Remove(playerId);
