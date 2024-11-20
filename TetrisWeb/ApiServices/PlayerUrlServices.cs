2ef1523bbd205fa5970e7f508dfce49a70ea21cc
happy <ricardo_ruizjr@hotmail.com>
2024-11-19 17:04:07 -0800

    Users can now have profile pics no links

---
+++ TetrisWeb/ApiServices/PlayerUrlServices.cs
@@ -0,0 +1,22 @@
+﻿namespace TetrisWeb.ApiServices
+{
+    public static class PlayerUrlServices
+    {
+        public static string ToDataUrl(this MemoryStream data, string format)
+        {
+            var span = new Span<byte>(data.GetBuffer()).Slice(0, (int)data.Length);
+            return $"data:{format};base64,{Convert.ToBase64String(span)}";
+        }
+
+        public static byte[] ToBytes(string url)
+        {
+            var commaPos = url.IndexOf(',');
+            if (commaPos >= 0)
+            {
+                var base64 = url.Substring(commaPos + 1);
+                return Convert.FromBase64String(base64);
+            }
+            return null;
+        }
+    }
+}
