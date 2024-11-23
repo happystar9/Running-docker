cd62d6c9aa481fddcd953bd6bb798627529492fb
Audrey Inglish <audreyinglish@gmail.com>
2024-11-23 12:56:31 -0700

    restructured integration testing suite

---
+++ TetrisTest/PlayerControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using TetrisWeb.DTOs;
+using Xunit.Abstractions;
+using FluentAssertions;
+
+namespace TetrisTest;
+
+
+public class PlayerControllerTests : PostgresTestBase
+{
+    public PlayerControllerTests(WebApplicationFactory<Program> webAppFactory, ITestOutputHelper outputHelper)
+        : base(webAppFactory, outputHelper) { }
+
+    [Fact]
+    public async Task RegisterPlayerCreatesPlayer()
+    {
+        var client = CustomWebAppFactory.CreateClient();
+
+        var samplePlayer = new PlayerDto
+        {
+            Username = "Testing99",
+            Authid = "Testing99",
+            PlayerQuote = "TestQuote",
+            AvatarUrl = "TestAvatarUrl",
+            Isblocked = false
+        };
+
+        var response = await client.PostAsJsonAsync("/api/player/register", samplePlayer);
+        response.EnsureSuccessStatusCode();
+
+        var createdPlayer = await response.Content.ReadFromJsonAsync<PlayerDto>();
+
+        createdPlayer.Should().NotBeNull();
+        createdPlayer.Username.Should().Be(samplePlayer.Username);
+        createdPlayer.Authid.Should().Be(samplePlayer.Authid);
+        createdPlayer.PlayerQuote.Should().Be(samplePlayer.PlayerQuote);
+        createdPlayer.AvatarUrl.Should().Be(samplePlayer.AvatarUrl);
+        createdPlayer.Isblocked.Should().BeFalse();
+        createdPlayer.Id.Should().BeGreaterThan(0);
+    }
+}
---
+++ TetrisTest/PlayerServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using TetrisWeb.DTOs;
+using Xunit.Abstractions;
+using TetrisWeb.Services;
+using TetrisWeb.ApiServices.Interfaces;
+
+namespace TetrisTest;
+
+public class PlayerServiceTests : PostgresTestBase
+{
+    public PlayerServiceTests(WebApplicationFactory<Program> webAppFactory, ITestOutputHelper outputHelper)
+        : base(webAppFactory, outputHelper) { }
+
+    [Fact]
+    public async Task CanRegisterPlayerUsingService()
+    {
+        var playerService = GetService<IPlayerService>();
+
+        var samplePlayer = new PlayerDto
+        {
+            Username = "Testing98",
+            Authid = "Testing98",
+            PlayerQuote = "TestQuote",
+            AvatarUrl = "TestAvatarUrl",
+            Isblocked = false
+        };
+
+        var createdPlayer = await playerService.CreatePlayerAsync(samplePlayer);
+
+        createdPlayer.Should().NotBeNull();
+        createdPlayer.Username.Should().Be(samplePlayer.Username);
+        createdPlayer.Authid.Should().Be(samplePlayer.Authid);
+        createdPlayer.PlayerQuote.Should().Be(samplePlayer.PlayerQuote);
+        createdPlayer.AvatarUrl.Should().Be(samplePlayer.AvatarUrl);
+        createdPlayer.Isblocked.Should().BeFalse();
+        createdPlayer.Id.Should().BeGreaterThan(0);
+    }
+}
+
