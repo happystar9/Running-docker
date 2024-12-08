bd74b7a730431eaa7329f5673a5c31c22ebbd1dd
Audrey Inglish <audreyinglish@gmail.com>
2024-12-08 14:37:36 -0700

    changed up score service return types

--- TetrisWeb/ApiServices/Interfaces/IScoreService.cs
+++ TetrisWeb/ApiServices/Interfaces/IScoreService.cs
@@ -5,14 +5,14 @@ namespace TetrisWeb.ApiServices.Interfaces;
 
 public interface IScoreService
 {
-    public int GetPlayerHighScore(int playerId);
+    public Task<int> GetPlayerHighScore(int playerId);
 
     //takes in a new score and checks to see if it is higher than the player's current high score
     //will call GetPlayerHighScore
-    public bool IsHighScore(int playerId, int score);
+    //private bool IsHighScore(int playerId, int score);
 
     //calls IsHighScore and if true, will update the player's high score
-    public int UpdateHighScore(int playerId, int score);
+    public Task<int> UpdateHighScore(int playerId, int score);
 
     public Task<List<LeaderboardDto>> GetTopLeaderboardItemsAsync();
 
