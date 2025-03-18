45451e86522027202a3ca139be19cd1471a8087e
happy <ricardo_ruizjr@hotmail.com>
2025-03-18 10:43:07 -0600

    Prometheus sucks

--- TetrisWeb/Program.cs
+++ TetrisWeb/Program.cs
@@ -16,7 +16,10 @@ using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
 using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Diagnostics.Metrics;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Instrumentation.Http;
+using Prometheus;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -42,7 +45,7 @@ var connectionString = builder.Configuration["DB_CONN"] ?? throw new InvalidOper
 
 string apiBaseUrl = builder.Environment.IsDevelopment()
     ? "http://localhost:5247"
-    : builder.Configuration["ApiBaseUrl"];
+    : "https://tetrisweb.azurewebsites.net";
 
 builder.Services.AddHttpClient("TetrisApi", client =>
 {
@@ -117,26 +120,75 @@ builder.Logging.AddOpenTelemetry(options =>
                 .AddService(serviceName))
         .AddOtlpExporter(otlpOptions =>
         {
-            otlpOptions.Endpoint = new Uri("http://otel-collector:4317");  
-            otlpOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc; 
+            otlpOptions.Endpoint = new Uri("http://otel-collector:4317");
+            otlpOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.Grpc;
         })
         .AddConsoleExporter();
 });
 builder.Services.AddOpenTelemetry()
       .ConfigureResource(resource => resource.AddService(serviceName))
       .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
           .AddConsoleExporter())
       .WithMetrics(metrics => metrics
-          .AddConsoleExporter());
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddMeter("http_requests_total", "player_score", "active_players", "api_response_time")
+        .AddPrometheusExporter());
+
 
 var app = builder.Build();
 
 var logger = app.Services.GetRequiredService<ILogger<Program>>();
 logger.LogInformation("Service Name: {ServiceName} - Logging Initialized", serviceName);
 
+var meter = new Meter(serviceName);
+
+var requestCounter = meter.CreateCounter<long>("http_requests_total", "requests", "The total number of HTTP requests made.");
+var requestCounter1 = meter.CreateObservableCounter<long>("http_requests_total", () => 0, "The total number of HTTP requests made.");
+var playerScoreGauge = meter.CreateGauge<long>("player_score", "The score of the player");
+var activePlayersCounter = meter.CreateUpDownCounter<int>("active_players");
+var apiResponseTimeHistogram = meter.CreateHistogram<double>("api_response_time", "The response time of the API in milliseconds");
+
+Task.Run(async () =>
+{
+    while (true)
+    {
+        var activePlayers = new Random().Next(0, 100);
+        var responseTime = new Random().NextDouble() * 500;
+        var playerScore = new Random().Next(0, 1000);
+
+        requestCounter.Add(1);
+
+        logger.LogInformation("Metric: http_requests_total, Value: {Value}", 1);
+        logger.LogInformation("Metric: active_players, Value: {Value}", activePlayers);
+        logger.LogInformation("Metric: api_response_time, Value: {Value}", responseTime);
+        logger.LogInformation("Metric: player_score, Value: {Value}", playerScore);
+
+        playerScoreGauge.Record(playerScore);
+        activePlayersCounter.Add(activePlayers);
+        apiResponseTimeHistogram.Record(responseTime);
+
+        await Task.Delay(5000); 
+    }
+}); 
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseAntiforgery();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapMetrics();
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseMigrationsEndPoint();
 }
 else
@@ -146,16 +198,15 @@ else
     app.UseHsts();
 }
 
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-
 app.UseCors("AllowSpecificOrigin");
 app.UseHttpsRedirection();
-app.UseAuthorization();
 app.MapControllers();
 
 
